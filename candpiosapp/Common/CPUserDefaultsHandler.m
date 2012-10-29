ef5aa68096abc4d19069d614040c9c9fff4a6c0b
Kevin Utecht <kevin@kutecht.com>
2012-10-29 08:53:58 -0500

    added monitoring of the checkout time with added benifit of performing 5min and checkout notifications when they happen.

--- candpiosapp/Common/CPUserDefaultsHandler.m
+++ candpiosapp/Common/CPUserDefaultsHandler.m
@@ -17,6 +17,7 @@
 [[NSUserDefaults standardUserDefaults] synchronize];\
 } while (0)
 
+
 @implementation CPUserDefaultsHandler
 
 NSString* const kUDCurrentUser = @"loggedUser";
@@ -109,13 +110,104 @@ NSString* const kUDPastVenues = @"pastVenues";
     return DEFAULTS(object, kUDPastVenues);
 }
 
+#define kCheckoutMonitorDuration 20.0
+#define kWarnBeforeCheckoutInMin  5.0
+#define kSecondsPerMinute          60
 
 NSString* const kUDCheckoutTime = @"localUserCheckoutTime";
+NSString* const kUDCheckoutTimeWarned = @"localUserCheckoutTimeWarned";
+NSString* const kUDCheckoutTimeReached = @"localUserCheckoutTimeReached";
+
++ (void)monitorCheckoutTime
+{
+    BOOL hasCheckoutBeenReached = [DEFAULTS(object, kUDCheckoutTimeReached) boolValue];
+    if ([CPUserDefaultsHandler isUserCurrentlyCheckedIn])
+    {
+#if DEBUG
+        NSLog(@"%g minutes till checkout", ([CPUserDefaultsHandler checkoutTime] - [[NSDate date]timeIntervalSince1970])/kSecondsPerMinute);
+#endif
+        
+        // Display warning
+        BOOL hasBeenWarned = [DEFAULTS(object, kUDCheckoutTimeWarned) boolValue];
+        if ((!hasBeenWarned) && ((([CPUserDefaultsHandler checkoutTime] - [[NSDate date]timeIntervalSince1970])/kSecondsPerMinute) < kWarnBeforeCheckoutInMin))  {
+            
+            //
+            // warn user about checkout happening soon
+            //
+#if DEBUG            
+            NSLog(@"Sending warning that checkout to happen soon.");
+#endif
+            
+            UILocalNotification *localNotif = [[UILocalNotification alloc] init];
+            
+            CPVenue *venue = [self currentVenue];
+            int minutesLeft = (int)(([CPUserDefaultsHandler checkoutTime] - [[NSDate date]timeIntervalSince1970])/60);
+            if (venue) {
+                localNotif.alertBody = [NSString stringWithFormat:@"You will be checked out of %@ in %d minutes.",
+                                        venue.name, minutesLeft];
+            }
+            else {
+                localNotif.alertBody = [NSString stringWithFormat:@"You will be checked out in %d minutes.", minutesLeft];
+            }
+            localNotif.alertAction = @"Check Out";
+            localNotif.soundName = UILocalNotificationDefaultSoundName;
+            
+            // since it is undetermined which thread calls monitorCheckoutTime, for safety doing the notify on main thread
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [[UIApplication sharedApplication] presentLocalNotificationNow:localNotif];
+            });
+            
+            SET_DEFAULTS(Object, kUDCheckoutTimeWarned, [NSNumber numberWithBool:YES]);
+        }
+        
+        
+        
+        [self performSelector:@selector(monitorCheckoutTime) withObject:nil afterDelay:kCheckoutMonitorDuration];
+    }
+    else if (!hasCheckoutBeenReached) {
+        
+        //
+        // send notification for action menu to update image for the checkin button
+        //
+        
+#if DEBUG
+        NSLog(@"Sending notifiection that checkin state changed.");
+#endif
+
+        // since it is undetermined which thread calls monitorCheckoutTime, for safety doing the notify on main thread
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [[NSNotificationCenter defaultCenter] postNotificationName:@"userCheckInStateChange" object:nil];
+        });
+        
+        SET_DEFAULTS(Object, kUDCheckoutTimeReached, [NSNumber numberWithBool:YES]);
+    }
+}
 
 + (void)setCheckoutTime:(NSInteger)checkoutTime
 {
-    // set the NSUserDefault to the user checkout time
-    SET_DEFAULTS(Object, kUDCheckoutTime, [NSNumber numberWithInt:checkoutTime]);
+    NSInteger storedCheckoutTime = [self checkoutTime];
+    
+    if (checkoutTime != storedCheckoutTime)
+    {
+        // checkoutTime has changed, reset warning flag
+        
+        // set the NSUserDefault to the user checkout time
+        SET_DEFAULTS(Object, kUDCheckoutTime, [NSNumber numberWithInt:checkoutTime]);
+        SET_DEFAULTS(Object, kUDCheckoutTimeWarned, [NSNumber numberWithBool:NO]);
+    }
+    
+    if (checkoutTime > [[NSDate date]timeIntervalSince1970])
+    {
+        SET_DEFAULTS(Object, kUDCheckoutTimeReached, [NSNumber numberWithBool:NO]);
+    }
+    else
+    {
+        SET_DEFAULTS(Object, kUDCheckoutTimeReached, [NSNumber numberWithBool:YES]);
+    }
+    
+    // always start montioring even if chechoutTime unchanged -- needed when app restarts before user checked out.
+    [self cancelPreviousPerformRequestsWithTarget:self selector:@selector(monitorCheckoutTime) object:nil];
+    [self performSelector:@selector(monitorCheckoutTime) withObject:nil afterDelay:kCheckoutMonitorDuration];
 }
 
 + (NSInteger)checkoutTime
